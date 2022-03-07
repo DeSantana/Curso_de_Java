006a2853332241801d7738278d8eaf6ef916aa16
João Victor <85123879+victorlive@users.noreply.github.com>
2022-03-06 23:00:28 -0300

    Create Aviao.java

---
+++ Orientação a Objetos/Aviao.java
@@ -0,0 +1,16 @@
+package POO;
+
+public class Aviao  extends Carro{
+
+	double envergadura; 
+	
+	
+	void aterrizar () {
+		System.out.println("------------____________");
+	}
+	
+	void acelerar () {
+		System.out.println("____________-------------");
+	}
+
+}
